ae892ee55baf659d044112643bb261a75a277706
Maja Zuban <mzuban@unipu.hr>
2023-07-28 16:05:27 +0200

    prvi upload vue_04

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: Home,
+  },
+  {
+    path: "/login",
+    name: "login",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Login.vue"),
+  },
+  {
+    path: "/signup",
+    name: "signup",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Signup.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
---
+++ src/views/Signup.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="about">
+    <h1>This is an login page</h1>
+
+    <div class="row">
+      <div class="col"></div>
+      <div class="col">
+        <form>
+          <div class="form-group">
+            <label for="exampleInputName1">Ime</label>
+            <input
+              type="name"
+              v-model="name"
+              class="form-control"
+              id="exampleInputName1"
+              placeholder="Enter name" />
+          </div>
+
+          <div class="form-group">
+            <label for="exampleInputSurname1">Prezime</label>
+            <input
+              type="surname"
+              v-model="surname"
+              class="form-control"
+              id="exampleInputSurname1"
+              placeholder="Enter surname" />
+          </div>
+
+          <div class="form-group">
+            <label for="exampleInputEmail1">E-mail adresa</label>
+            <input
+              type="email"
+              v-model="username"
+              class="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+              autocomplete="username" />
+            <small id="emailHelp" class="form-text text-muted">
+              We'll never share your email with anyone else.
+            </small>
+          </div>
+
+          <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input
+              type="password"
+              v-model="password"
+              class="form-control"
+              id="exampleInputPassword1"
+              placeholder="Password"
+              autocomplete="current-password" />
+          </div>
+
+          <button type="button" @click="signup()" class="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+      <div class="col"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import firebase from "@/firebase";
+
+export default {
+  name: "Signup",
+  data() {
+    return {
+      name: "",
+      surname: "",
+      username: "",
+      password: "",
+    };
+  },
+
+  methods: {
+    signup() {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.username, this.password)
+        .then(function () {
+          console.log("Uspješna registracija");
+        })
+        .catch(function (error) {
+          console.error("Došlo je do greške", error);
+        });
+      console.log("Nastavak.");
+    },
+  },
+};
+</script>
